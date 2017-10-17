3c5a4083f2753c59da1955df71e5f4376747c6b3
mixassio <mixail.bondarev@gmail.com>
2017-10-17 17:41:26 +0300

    ended db with foodS

--- download.py
+++ download.py
@@ -1,4 +1,3 @@
-import re
 import requests
 import bs4
 import sqlite3
@@ -6,19 +5,14 @@ import sqlite3
 conn = sqlite3.connect('./food.db')
 c = conn.cursor()
 c.execute("DROP TABLE  foods_link")
-c.execute("CREATE TABLE foods_link (link CHAR, title CHAR)")
+c.execute("CREATE TABLE foods_link (page CHAR, link CHAR, title CHAR)")
 
-list_foods = []
-for page in range(1, 10):
+for page in range(1, 1000000):
     url = 'https://eda.ru/recepty?page={}'.format(page)
     foods_html = requests.get(url)
     soup = bs4.BeautifulSoup(foods_html.content, "lxml")
     for food in soup('h3', {'class': 'horizontal-tile__item-title item-title'}):
-        print(food.a.span.string)
-        print(food.a['href'])
-        list_foods.append([food.a['href'], food.a.span.string])
-
-for line in list_foods:
-    c.execute("insert into foods_link values (?, ?)", line)
-    conn.commit()
-    print(line)
+        c.execute("insert into foods_link values (?, ?, ?)", [page, food.a['href'], food.a.span.string])
+    if page % 100 == 0:
+        conn.commit()
+        print(page)
